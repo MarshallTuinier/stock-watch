5662c4f67bfa9f935882f46813a67d0606e53d73
Marshall Tuinier <mtuinier@gmail.com>
2017-09-27 10:21:11 -0700

    Background Color Change

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { LinearGradient } from '@vx/gradient';
+import { GradientPurpleOrange } from '@vx/gradient';
 
 const Background = ({ width, height }) => {
   return (
     <svg width={width} height={height}>
-      <LinearGradient id="fill" vertical={false}>
-        <stop stopColor="#a943e4" offset="0%" />
-        <stop stopColor="#f55989" offset="50%" />
-        <stop stopColor="#ffaf84" offset="100%" />
-      </LinearGradient>
+      <GradientPurpleOrange id="fill" vertical={false} />
 
       <rect width={width} height={height} fill="url(#fill)" />
     </svg>
